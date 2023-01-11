cb59efaf49d885c26ccc7507dd88d8c0259d181c
Sohaib ZQ <sohaib429@gmail.ccom>
2023-01-10 20:35:11 -0800

    barchar_bug_fixed

--- src/Components/Barcharts.js
+++ src/Components/Barcharts.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Draw_bar from './Draw_bar'
 import Stacked_bar from './Stacked_bar'
+import json_data from "./Json_data/Json_file.json"
+import { useState } from 'react'
 
 export default function Barcharts(props) {
+  const [localdata,setlocaldata]=useState(json_data)
+  const sites=Array.from(localdata).map((c)=>{return  c.name})
+  const nar=Array.from(localdata).map((c)=>{return  c.nar})
+ 
   return (
     <div style={{display: "flex",flexDirection:'row'}}>
         <br/>
-        <div align='center' style={{width : '90%',top:'160'}}><Stacked_bar/></div>
-       {/*<div align='right' style={{width : '40%',top:'160'}}><Barcharts sites={props.sites} nars={props.nar} /></div>*/}
+        <div align='center' style={{width : '95%',top:'160'}}><Stacked_bar/></div>
+       <div style={{width : '95%',top:'160'}}><Draw_bar sites={sites} nars={nar} /></div>
       {/*i tried to display the bar garh but God what reasons the browser freez*/}
     </div>
   )
