241ac8103b12b29775227de906aad391ed0e5efb
Sohaib ZQ <sohaib429@gmail.ccom>
2023-01-15 02:47:24 -0800

    barcharts responsiveness activated

--- src/Components/Barcharts.js
+++ src/Components/Barcharts.js
@@ -3,18 +3,23 @@ import Draw_bar from './Draw_bar'
 import Stacked_bar from './Stacked_bar'
 import json_data from "./Json_data/Json_file.json"
 import { useState } from 'react'
-
+import { Grid } from '@mui/material'
 export default function Barcharts(props) {
   const [localdata,setlocaldata]=useState(json_data)
   const sites=Array.from(localdata).map((c)=>{return  c.name})
   const nar=Array.from(localdata).map((c)=>{return  c.nar})
  
   return (
-    <div style={{display: "flex",flexDirection:'row'}}>
-        <br/>
-        <div align='center' style={{width : '95%',top:'160'}}><Stacked_bar/></div>
-       <div style={{width : '95%',top:'160'}}><Draw_bar sites={sites} nars={nar} /></div>
+  
+    <Grid container spacing={1}>
+      <Grid item xs = {12} sm={12} md={3} xl={3}>
+      <Stacked_bar/>
+      </Grid>
+      <Grid item xs = {12}  sm={12} md={9} xl={9}>
+    <Draw_bar sites={sites} nars={nar} />
+      </Grid>
+    
       {/*i tried to display the bar garh but God what reasons the browser freez*/}
-    </div>
+      </Grid> 
   )
 }
